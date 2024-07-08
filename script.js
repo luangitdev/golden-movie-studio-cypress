70ab0ecf387f6ec17633bb54a05391ed668269a9
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2024-07-08 16:16:24 -0300

    server prod3

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const API_KEY = 'f7f22d30'; 
-const localURL = 'http://localhost:3000/cadastro';
-const productionURL = 'https://8i5962r1e8.execute-api.us-east-1.amazonaws.com';
+//const API_URL = 'http://localhost:3000/cadastro';
+const API_URL = 'https://8i5962r1e8.execute-api.us-east-1.amazonaws.com/cadastro';
 
-const API_URL = window.location.hostname === 'localhost' ? localURL : productionURL;
 
 document.addEventListener('DOMContentLoaded', fetchRecommendations);
 document.getElementById('search-button').addEventListener('click', searchMovies);
