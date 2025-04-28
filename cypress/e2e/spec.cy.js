fdc2ac3d5459633c85e98bbc611689d6f51acd38
luangitdev <luaninveste@gmail.com>
2025-04-28 20:52:25 -0300

    feat: atualizar arquivo spec.cy.js com os parâmetros necessários ao teste.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('US-012-Funcionalidade: Cadastro de membros', () => {
+  it('Deve fazer o cadastro de campos obrigatórios', () => {
+    cy.visit('http://127.0.0.1:8080/')
+    cy.get('#signup-firstname').type('Milano')
+    cy.get('#signup-lastname').type('Castro')
+    cy.get('#signup-email').type('castro@gmail.com')
+    cy.get('#signup-phone').type('11999999999')
+    cy.get('#signup-password').type('Teste@123')
+    cy.get('#signup-button').click()
+    cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!')
   })
 })
